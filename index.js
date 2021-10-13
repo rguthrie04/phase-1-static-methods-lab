047dd1c18bd05147f4e6e675a3408f282e4b2599
Ryan Guthrie <ryanguthrie@zoho.com>
2021-10-13 11:52:52 +0100

    commit tree 44c9b5e1013ab3da8f4a20d644aeb8eab0960d1d

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(capWord) {
+    return capWord.charAt(0).toUpperCase()+capWord.slice(1)
+  }
+
+  static sanitize(removeNums) {
+    return removeNums.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(sentence) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const final = []
+    const arrayOfWords = sentence.split(" ")
+        for (let i = 0; i < arrayOfWords.length; i++){
+      if( i == 0) {
+        final.push(this.capitalize(arrayOfWords[i]))
+      } else {
+        if (exceptions.includes(arrayOfWords[i])){
+          final.push(arrayOfWords[i])
+          } else{
+            final.push(this.capitalize(arrayOfWords[i]))
+          }
+        }
+      }
+      return final.join(" ")
+      
+}
 }
